b6900f8963947ab39b4cddcba1c97a43fdd669c5
Starenchenko Maksym <36163370+Gostb@users.noreply.github.com>
2024-03-09 13:28:14 +0200

    cart styles (#4)

--- src/components/Cart/cart-info/cartInfo.js
+++ src/components/Cart/cart-info/cartInfo.js
@@ -1,18 +1,18 @@
 import { NavLink } from 'react-router-dom'
 
 const CartInfo = () => {
-    return(
+    return (
         <section className="info">
-        <div className="container">
-            <div className="info__navigation">
-                <NavLink to="/Main">Home</NavLink>
-                <div>My account</div>
+            <div className="container">
+                <div className="info__navigation">
+                    <NavLink to="/Main">Home</NavLink>
+                    <div>My account</div>
+                </div>
+                <div className="info__text">
+                    <p>Please fill in the fields below and click place order to complete your purchase!</p>
+                    <p>Already registered? <a href="#">Please login here</a></p>
+                </div>
             </div>
-            <div className="info__text">
-                <p>Please fill in the fields below and click place order to complete your purchase!</p>
-                <p>Already registered? <a href="#">Please login here</a></p>
-            </div>
-        </div>
         </section>
     )
 }
